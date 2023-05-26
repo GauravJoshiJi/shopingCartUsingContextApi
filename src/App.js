df15e6cc8d3aa24551e5e06ca417e6eb73ff7e7e
GauravJoshiJi <gauravjoshi1997.joshi@gmail.com>
2023-05-26 14:50:28 +0530

    Addition and Substraction functionaligy

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import './App.css';
-import { useState } from 'react';
-import Items from './components/Items';
-import Navbar from './components/Navbar';
-
+import "./App.css";
+import { useState } from "react";
+import Items from "./components/Items";
+import Navbar from "./components/Navbar";
+import { itemContext } from "./ItemContext";
 
 function App() {
   const [total, setTotal] = useState(0);
   const [item, setItem] = useState(0);
   return (
-        <div className='App'>
-          <h2>Shopping Cart</h2>
-            <Navbar />
-            <Items />
-        </div>
+    <itemContext.Provider
+      value={{
+        total: total,
+        setTotal: setTotal,
+        item: item,
+        setItem: setItem,
+      }}
+    >
+      <div className="App">
+        <h2>Shopping Cart</h2>
+        <Navbar />
+        <Items />
+      </div>
+    </itemContext.Provider>
   );
 }
 export default App;
--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import styles from "../styles/ItemCard.module.css";
+import { useContext } from "react";
+import { itemContext } from "../ItemContext";
 
-function ItemCard({ name, price }) {
-  const handleAdd = () => {};
+function ItemCard({ id, name, price }) {
+  const value = useContext(itemContext);
+  const { total, setTotal, item, setItem } = value; //DStructuring
+  const handleAdd = () => {
+    setTotal(total + price);
+    setItem(item + 1);
+  };
 
-  const handleRemove = () => {};
+  const handleRemove = () => {
+    if (total <= 0) {
+      return;
+    }
+    setTotal((prevState) => prevState - price);
+    setItem(item - 1);
+  };
 
   return (
     <div className={styles.itemCard}>
